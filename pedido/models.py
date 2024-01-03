ba02ee82062ffd995e0ac407630bc15fae225f88
Eduardo lobo <63437120+wolfxweb@users.noreply.github.com>
2024-01-03 15:55:19 -0300

    #pedido

--- pedido/models.py
+++ pedido/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class Pedido(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     total = models.FloatField()
-
+    qtd_total = models.PositiveIntegerField( blank=True, null=True)
     STATUS_CHOICES = [
         ("A", "Aprovado"),
         ("C", "Criado"),
@@ -35,7 +35,8 @@ class ItemPedido(models.Model):
     preco = models.FloatField()
     quantidade = models.PositiveBigIntegerField()
     imagen = models.CharField(max_length = 20000)
-
+    preco_promocional = models.FloatField(default=0, blank=True, null=True)
+    imagem = models.CharField(max_length=2000, blank=True, null=True)
     def __str__(self) -> str:
         return f"Item do {self.pedido}"
     class Meta:
--- produto/views.py
+++ produto/views.py
@@ -150,4 +150,28 @@ class CarrinhoProdutos(View):
 
 class ResumoCompra(View):
     def get(self, *args, **kwargs):
-        return HttpResponse('FinalizarCarrinho')
+        if not self.request.user.is_authenticated:
+            return redirect('perfil:criar')
+
+        perfil = Perfil.objects.filter(usuario=self.request.user).exists()
+
+        if not perfil:
+            messages.error(
+                self.request,
+                'Usuário sem perfil.'
+            )
+            return redirect('perfil:criar')
+
+        if not self.request.session.get('carrinho'):
+            messages.error(
+                self.request,
+                'Carrinho vazio.'
+            )
+            return redirect('produto:lista')
+
+        contexto = {
+            'usuario': self.request.user,
+            'carrinho': self.request.session['carrinho'],
+        }
+
+        return render(self.request, 'produto/resumodacompra.html', contexto)
